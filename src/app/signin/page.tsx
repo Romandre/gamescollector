c5a4a11b2529cff3836dce6f3102dad00b20c2a2
Roman Kucherenko <roman.kucherenko@vaimo.com>
2025-01-04 14:54:33 +0100

    Created signin route

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,10 @@
+import { Header, Container } from "@/components";
+
+export default async function Signin() {
+  return (
+    <>
+      <Header />
+      <Container>Sign in is not ready yet...</Container>
+    </>
+  );
+}
